774f1491268666d761c3cb2b0aeb7293c6f2106c
sank28 <ssd20072@gmail.com>
2015-09-09 15:00:48 -0500

    stuff

--- code.h
+++ code.h
@@ -1,6 +1,7 @@
 #ifndef _code_h
 #define _code_h
 
+void execute(char *name, char *params, void codeCallback(bool error, char *result));
 void executeCodeServiceWithoutParams(char *serviceName, void (*codeCallback)(bool error, char *result));
 void executeCodeServiceWithParams(char *serviceName, char *params, void (*codeCallback)(bool error, char *result));
 
