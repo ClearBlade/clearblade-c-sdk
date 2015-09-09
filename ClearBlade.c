411f98d00c8e0a8fbb4a4c249e9a6f1ffedbc960
sank28 <ssd20072@gmail.com>
2015-09-09 15:02:51 -0500

    removed main method

--- ClearBlade.c
+++ ClearBlade.c
@@ -7,16 +7,7 @@
 #include "request_engine.h"
 #include "util.h"
 #include "user.h"
-#include "code.h"
-
-struct ClearBlade {
-	char *systemKey;
-	char *systemSecret;
-	char *platformURL;
-	char *messagingURL;
-	char *email;
-	char *password;
-};
+#include "clearblade.h"
 
 void validateInitOptions(struct ClearBlade *CB) {
 	if (CB->systemKey == NULL) {
@@ -75,34 +66,4 @@ void initializeClearBlade(char *systemkey, char *systemsecret, char *platformurl
 	CB->password = userPassword;
 
 	initialize(CB, initCallback);
-}
-
-void logoutCallback(bool error, char *message) {
-	if (!error) {
-		printf("Logout: %s\n", message);
-	}
-}
-
-void codeCallback(bool error, char *message) {
-	if (error) {
-		printf("ERROR: %s\n", message);
-	} else {
-		printf("SUCCESS: %s\n", message);
-	}
-}
-
-void initCallback(bool error, char *message) {
-	if (error) {
-		printf("ERROR: %s\n", message);
-	} else {
-		printf("SUCCESS: %s\n", message);
-	}
-}
-
-int main(int argc, char *argv[]) {
-	initializeClearBlade("bccba9de0a94e4daef8ed189aff801", "BCCBA9DE0AF2BAF39FB1EFB0CE47", "https://rtp.clearblade.com", "rtp.clearblade.com:1883", "pi@clearblade.com", "clearblade", initCallback);
-	executeCodeServiceWithoutParams("test", codeCallback);
-	logoutUser(logoutCallback);
-
-	return 0;
 }
