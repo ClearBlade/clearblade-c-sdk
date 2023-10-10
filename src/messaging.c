c8ad4b2f723cdd075036ebe1c40468b44f72d330
Jim Bouquet <jbouquet@clearblade.com>
2023-10-10 14:05:11 -0500

    Added the ability to pass context for connect and disconnect

--- src/messaging.c
+++ src/messaging.c
@@ -8,27 +8,55 @@
 #include "messaging.h"
 
 MQTTAsync mqttClient = NULL;
-int finished = 0;
+//int finished = 0;
 char *clientID = "";
 int qos = 0;
 
+CbMqttConnectOptions* getDefaultConnectOptions() {
+	CbMqttConnectOptions* options;
 
-void onConnectFailure(void* context, MQTTAsync_failureData* response) {
-	printf("Connect failed, rc %d\n", response ? response->code : 0);
+	//These defaults are inline with what paho uses in the MQTTAsync_connectOptions_initializer
+	options->keepAliveInterval = 60;
+	options->cleanSession = true;
+	options->maxInFlight = 65535;
+	options->connectTimeout = 30;
+	options->retryInterval = 0;
+	options->automaticReconnect = false;
+	options->minRetryInterval = 1;
+	options->maxRetryInterval = 60;
+
+	return &options;
 }
 
+CbMqttDisconnectOptions* getDefaultDisconnectOptions() {
+	CbMqttDisconnectOptions* options;
+
+	//These defaults are inline with what paho uses in the MQTTAsync_disconnectOptions_initializer
+	options->timeout = 0;
+	options->onSuccess = NULL;
+	options->onFailure = NULL;
+
+	return &options;
+}
+
+void onConnectSuccess(void *context, MQTTAsync_successData *response) {
+	printf("MQTT Connection successful\n");
+}
+
+void onConnectFailure(void* context, MQTTAsync_failureData* response) {
+	printf("MQTT Connect failed, rc %d\n", response ? response->code : 0);
+}
 
 void connLost(void *context, char *cause) {
-	printf("\nConnection lost\n");
+	printf("\nMQTT Connection lost\n");
 	printf("Cause: %s\n", cause);
-	printf("Reconnecting\n");
-	connectToMQTT(clientID, qos, NULL, NULL);
 }
 
 
 void connectToMQTTAdvanced(char *clientId, int qualityOfService, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
  									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message),
 									void (*onConnLostCallback)(void *context, char *cause), bool autoReconnect) {
+	
 	if (getUserToken() == NULL && getDeviceToken() == NULL) {
 		fprintf(stderr, "connectToMQTT called with unset auth token\n");
 		return;
@@ -69,52 +97,117 @@ void connectToMQTTAdvanced(char *clientId, int qualityOfService, void (*mqttOnCo
 	conn_opts.context = client;
 	conn_opts.username = username;
 	conn_opts.password = password;
-	
-	// Note URL contains protocol before first colon (e.g. tcp://platform.clearblade.com:1883)
-	char *addrOfFirstColon;
-	addrOfFirstColon = strchr(messagingurl,':');
-	int relLocOfFirstColon = (addrOfFirstColon - messagingurl) * sizeof(char);
-    
-	// Allocate array for protocol and retrieve it from messagingurl; add null at end
-	char protocol[relLocOfFirstColon + 1];
-	strncpy(protocol, messagingurl, relLocOfFirstColon);
-	protocol[relLocOfFirstColon] = '\0';
 
 	// If protocol is 'ssl' TLS is desired
-	if (strcmp(protocol, "ssl") == 0) {
+	if (strstr(messagingurl, "ssl://") == messagingurl) {
 		MQTTAsync_SSLOptions ssl_opts = MQTTAsync_SSLOptions_initializer;
 		conn_opts.ssl = &ssl_opts;
 	};
 
   if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS) {
   	printf("Failed to start connect, return code %d\n", rc);
-	MQTTAsync_destroy(&client);
+		MQTTAsync_destroy(&client);
     return;
   }
 
-	while(finished == 0) {
+	//while(finished == 0) {
 
-	}
+	//}
 
 	mqttClient = client;
-	finished = 0;
+	//finished = 0;
 }
 
 void connectToMQTT(char *clientId, int qualityOfService, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
  									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message)) {
+
 	connectToMQTTAdvanced(clientId, qualityOfService, mqttOnConnect, messageArrivedCallback, NULL, false);
 }
 
 
+void connectCbMQTT(void* context, char *clientId, CbMqttConnectOptions *options, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
+ 									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message),
+									void (*onConnLostCallback)(void *context, char *cause)) {
+
+	//Use https://github.com/hivemq-cloud/paho-C-mqtt-client-example/blob/master/main.c as sample code
+	if (getUserToken() == NULL && getDeviceToken() == NULL) {
+		fprintf(stderr, "connectToMQTT called with unset auth token\n");
+		return;
+	}
+
+	const char *username = getUserToken();
+	if (username == NULL) {
+		username = getDeviceToken();
+	}
+	const char *password = getSystemKey();
+	char *messagingurl = getMessagingURL();
+
+	MQTTAsync client;
+	MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+	int rc;
+
+	MQTTAsync_create(&client, messagingurl, clientId, MQTTCLIENT_PERSISTENCE_NONE, context);
+
+	//Set callbacks
+	if(onConnLostCallback == NULL) {
+		MQTTAsync_setCallbacks(client, context, connLost, messageArrivedCallback, NULL);
+	} else {
+		MQTTAsync_setCallbacks(client, context, onConnLostCallback, messageArrivedCallback, NULL);
+	}
+
+	conn_opts.keepAliveInterval = options->keepAliveInterval;
+	conn_opts.cleansession = options->cleanSession;
+	conn_opts.maxInflight = options->maxInFlight;
+	conn_opts.connectTimeout = options->connectTimeout;
+	conn_opts.retryInterval = options->retryInterval;
+	conn_opts.automaticReconnect = options->automaticReconnect;
+	conn_opts.minRetryInterval = options->minRetryInterval;
+	conn_opts.maxRetryInterval = options->maxRetryInterval;
+
+	if (mqttOnConnect == NULL) {
+		conn_opts.onSuccess = onConnectFailure;
+	} else {
+		conn_opts.onSuccess = mqttOnConnect;
+	}
+
+	if (options->onFailure == NULL) {
+		conn_opts.onFailure = onConnectFailure;
+	} else {
+		conn_opts.onSuccess = options->onFailure;
+	}
+
+	conn_opts.context = context;
+	conn_opts.username = username;
+	conn_opts.password = password;
+
+	// If protocol is 'ssl' TLS is desired
+	if (strstr(messagingurl, "ssl://") == messagingurl) {
+		MQTTAsync_SSLOptions ssl_opts = MQTTAsync_SSLOptions_initializer;
+		conn_opts.ssl = &ssl_opts;
+	};
+
+	//Add the last will and testament options
+	if(options->will != NULL) {
+		conn_opts.will = options->will;
+	}
+
+	//Try to connect to the MQTT client
+  if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS) {
+  	printf("Failed to start connect, return code %d\n", rc);
+		MQTTAsync_destroy(&client);
+    return;
+  }
+}
+
 void onSubscribe(void* context, MQTTAsync_successData* response) {
 	printf("Subscribe succeeded\n");
-	finished = 1;
+	//finished = 1;
 }
 
 
 void onSubscribeFailure(void* context, MQTTAsync_failureData* response) {
 	printf("Subscribe failed, rc %d\n", response ? response->code : 0);
-	finished = 1;
+	//finished = 1;
 }
 
 
@@ -134,11 +227,11 @@ void subscribeToTopic(char *topic, int qos) {
 		return;
 	}
 
-	while(finished == 0) {
+	// while(finished == 0) {
 
-	}
+	// }
 
-	finished = 0;
+	// finished = 0;
 }
 
 
@@ -176,11 +269,13 @@ void unsubscribeFromTopic(char *topic) {
 }
 
 
-void onDisconnect(void* context, MQTTAsync_successData* response) {
-	printf("Successful disconnection\n");
-	finished = 1;
+void onDisconnectSuccess(void* context, MQTTAsync_successData* response) {
+	printf("MQTT disconnect success\n");
 }
 
+void onDisconnectFailure(void* context, MQTTAsync_failureData* response) {
+	printf("MQTT disconnect failed, rc %d\n", response ? response->code : 0);
+}
 
 void disconnectMQTTClient() {
 	if(mqttClient == NULL) {
@@ -190,17 +285,48 @@ void disconnectMQTTClient() {
 
 	int rc;
 	MQTTAsync_disconnectOptions disc_opts = MQTTAsync_disconnectOptions_initializer;
-	disc_opts.onSuccess = onDisconnect;
+	disc_opts.onSuccess = onDisconnectSuccess;
 
 	if((rc = MQTTAsync_disconnect(mqttClient, &disc_opts)) != MQTTASYNC_SUCCESS) {
 	        printf("Failed to start disconnect, return code %d\n", rc);
 	        return;
 	}
 	MQTTAsync_destroy(&mqttClient);
-	while (finished == 0) {
+	// while (finished == 0) {
 
-	}
+	// }
 
 	mqttClient = NULL;
-	finished = 0;
+	//finished = 0;
+}
+
+void disconnectCbMQTT(void* context, CbMqttDisconnectOptions *options) {
+	if(mqttClient == NULL) {
+		printf("You are not connected to the MQTT Broker. Please call connectToMQTT() first and then try to disconnect\n");
+		exit(-1);
+	}
+
+	int rc;
+	MQTTAsync_disconnectOptions disc_opts = MQTTAsync_disconnectOptions_initializer;
+	
+	disc_opts.timeout = options->timeout;
+
+	if (options->onSuccess == NULL) {
+		disc_opts.onSuccess = onDisconnectSuccess;
+	} else {
+		disc_opts.onSuccess = options->onSuccess;
+	}
+
+	if (options->onFailure == NULL) {
+		disc_opts.onFailure = onDisconnectFailure;
+	} else {
+		disc_opts.onSuccess = options->onFailure;
+	}
+
+	if((rc = MQTTAsync_disconnect(mqttClient, &disc_opts)) != MQTTASYNC_SUCCESS) {
+	        printf("Failed to start disconnect, return code %d\n", rc);
+	        return;
+	}
+	MQTTAsync_destroy(&mqttClient);
+	mqttClient = NULL;
 }
--- src/messaging.h
+++ src/messaging.h
@@ -3,12 +3,41 @@
 
 #include "MQTTAsync.h"
 
+typedef struct {
+  int keepAliveInterval;
+  bool cleanSession;
+  int maxInFlight;
+  int connectTimeout;
+  int retryInterval;
+	bool automaticReconnect;
+	int minRetryInterval;
+	int maxRetryInterval;
+	MQTTAsync_onSuccess* onSuccess;
+	MQTTAsync_onFailure* onFailure;
+	MQTTAsync_willOptions* will;
+
+} CbMqttConnectOptions;
+
+typedef struct {
+  int timeout;
+	MQTTAsync_onSuccess* onSuccess;
+	MQTTAsync_onFailure* onFailure;
+
+} CbMqttDisconnectOptions;
+
+
+CbMqttConnectOptions* getDefaultMQTTConnectOptions();
+
 void connectToMQTT(char *clientId, int qualityOfService, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
  									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message));
 
 void connectToMQTTAdvanced(char *clientId, int qualityOfService, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
  									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message),
 									void (*onConnLostCallback)(void *context, char *cause), bool autoReconnect);
+
+void connectCbMQTT(void* context, char *clientId, CbMqttConnectOptions *options, void (*mqttOnConnect)(void* context, MQTTAsync_successData* response),
+ 									int (*messageArrivedCallback)(void *context, char *topicName, int topicLen, MQTTAsync_message *message),
+									void (*onConnLostCallback)(void *context, char *cause));
 
 void subscribeToTopic(char *topic, int qos);
 
@@ -17,5 +46,7 @@ void publishMessage(char *message, char *topic, int qos, int retained);
 void unsubscribeFromTopic(char *topic);
 
 void disconnectMQTTClient();
+
+void disconnectCbMQTT(void* context, CbMqttDisconnectOptions *options);
 
 #endif
